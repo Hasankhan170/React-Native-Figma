50c4bd486fdebf2a66d3178189afb5b1ededa47b
Hasankhan170 <hasankhanali0316@gmail.com>
2024-10-20 20:38:33 +0500

    working

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,50 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { SafeAreaView, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text1}>Sign in</Text>
+      <View style={styles.container2}>
+      <Text>New User ?</Text>
+      <Link style={styles.text2} href={'createAccount'}>Create an Account</Link>
+      </View>
+      <TextInput
+      placeholder="Email Address"
+        style={styles.input}
+      />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    paddingTop: 40,
+    paddingHorizontal: 20,
+  },
+  text1:{
+    fontSize : 25,
+    fontWeight : '700',
+  },
+  container2:{
+    flexDirection: 'row',
+    gap: 5,
+    marginTop: 10,
+  },
+  text2:{
+    color :'#0098FF',
+    fontSize: 15,
+    fontWeight : '600',
+  },
+  input: {
+    height: 50,
+    marginBottom: 10,
+    borderBottomWidth: 1, // Bottom border ki width
+    borderBottomColor: '#000',
+    marginTop : 30
+  },
+  
+  
+
+})
